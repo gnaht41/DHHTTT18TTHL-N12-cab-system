6600c36e70e05d27424111cde7bdfb9a6ab28d43
ajf <doanthitramyt2004@gmail.com>
2026-03-04 07:11:16 +0700

    api-gateway

--- api-gateway/src/app.js
+++ api-gateway/src/app.js
@@ -1,15 +1,34 @@
+require('dotenv').config(); // Chèn thêm dòng này lên trên cùng
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
-
-const routes = require('./routes');
+const proxy = require('express-http-proxy');
+const authMiddleware = require('./middlewares/auth.middleware');
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
 
-app.use('/', routes);
+// Debug nhanh: In ra console để xem URL đã nhận được chưa
+console.log("Auth Service URL:", process.env.AUTH_SERVICE_URL);
+
+// Định tuyến request đến các service [1]
+app.use(
+  '/auth',
+  proxy(process.env.AUTH_SERVICE_URL || 'http://localhost:3001', {
+    proxyReqPathResolver: function (req) {
+      return '/auth' + req.url; 
+    }
+  })
+);
+app.use('/users', authMiddleware, proxy(process.env.USER_SERVICE_URL || 'http://localhost:3002'));
+app.use('/bookings', authMiddleware, proxy(process.env.BOOKING_SERVICE_URL || 'http://localhost:3004'));
+
+// Route test nội bộ
+app.get('/gateway/verify-test', authMiddleware, (req, res) => {
+    res.json({
+        message: 'JWT hợp lệ! Bạn đã đi qua API Gateway an toàn.',
+        user_payload: req.user
+    });
+});
 
 module.exports = app;
